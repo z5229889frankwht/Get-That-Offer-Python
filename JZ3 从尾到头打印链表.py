da7f7ef17d723e8c813d0619153bc73fdf71062f
FrankWangHT <61502604+z5229889frankwht@users.noreply.github.com>
2021-08-11 17:24:16 +1000

    1

--- JZ3 从尾到头打印链表.py
+++ JZ3 从尾到头打印链表.py
@@ -3,10 +3,21 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
-        
+
+
 class Solution:
     # 返回从尾部到头部的列表值序列，例如[1,2,3]
     def printListFromTailToHead(self, listNode):
-        if listNode is None: 
-            return [] 
-        return self.printListFromTailToHead(listNode.next) + [listNode.val]
+        #  方法一 使用栈的方式去实现
+        arrayList = []
+        while listNode:
+            arrayList.append(listNode.val)
+            listNode = listNode.next
+        return arrayList[::-1]
+
+        # 方法二 使用递归的方式去实现
+        # if listNode:
+        #     if listNode.next:
+        #         self.printListFromTailToHead(listNode.next)
+        #     self.arrayList.append(listNode.val)
+        # return self.arrayList
---
+++ JZ4 重建二叉树.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # 返回构造的TreeNode根节点
+    def reConstructBinaryTree(self, pre, vin):
+        # write code here
+        return []
